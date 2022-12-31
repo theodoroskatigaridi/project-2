71dcad6dc537e1a6029c62e962f0465c54f81f71
theodoroskatigaridi <118885367+theodoroskatigaridi@users.noreply.github.com>
2022-12-31 21:32:24 +0200

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="el">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Μετατροπή Χιλιόμετρα σε Μέτρα (km → m)</title>
+
+</head>
+<body class="js">
+	<header id="header" class="row">
+<div class="column-12 row">
+<div class="column-1 tablet-column-1 mobile-column-1 lang-trigger lang-menu">
+<div class="dropdown-toggle trigger"><i class="flag flag-el"></i> <i class="icon icon-down-open"></i></div>
+</div>
+<div class="column-9 tablet-column-2 hidden-mobile logo">
+<a href="/el/μετατροπή">μετατροπή</a>
+</div>
+<div class="hidden-desktop tablet-column-1 mobile-column-1 burger-menu main-menu"><span class="menu-trigger trigger"></span></div>
+</div>
+	</header>
+	<div id="main" class="row">
+		<section class="column-8">
+			<h1 class="unit-title">Χιλιόμετρα σε Μέτρα</h1>
+<div id="da-top"></div>
+<form id="converter" action="#" class="row">
+<div class="row value">
+	<div class="column-6 tablet-column-4 mobile-column-3 left">
+		<input tabindex="1" id="convert-value" type="number" step="any" />
+	</div>
+	<div class="column-2 tablet-column-2 mobile-column-1 center">
+		<input tabindex="2" id="convert-submit" type="submit" value="μετατροπή" />
+	</div>
+</div>
+<div class="row meta">
+	<div class="column-3 tablet-column-2 mobile-column-1 left"><label for="unit_from">Από</label></div>
+	<div class="column-2 tablet-column-2 mobile-column-2 swap-container">
+		<svg class="swap" aria-labelledby="title" role="img" viewBox="0 0 596 597">
+    <title>Αντιστροφή Μονάδων</title>
+    <desc>Αντιστροφή icon</desc>
+    <path class="arrows" d="M 102,204 0,306 102,408 91.766906,339.27715 l 188.323774,0 0.40932,-69.01024 -184.639856,0 z M 459,153 357,51 l 3.68391,60.12705 -181.77459,0 L 178.5,185.04918 365.18648,185.4585 357,255 Z"></path>
+</svg>	</div>
+	<div class="column-3 tablet-column-2 mobile-column-1 right"><label for="unit_to">Σε</label></div>
+</div>
+<div class="row units">
+	<div class="column-3 tablet-column-2 mobile-column-2 left">
+		<select tabindex="3" id="unit_from">
+							<option value="8" data-url="άνγκστρομ">Άνγκστρομ (Å)</option>
+							<option value="25" data-url="αστρονομικές-μονάδες">Αστρονομικές μονάδες (au)</option>
+							<option value="4" data-url="εκατοστά">Εκατοστά (cm)</option>
+							<option value="3" data-url="δεκατόμετρα">Δεκατόμετρα (dm)</option>
+							<option value="15" data-url="πόδια">Πόδια (ft)</option>
+							<option value="18" data-url="ίντσες">Ίντσες (in)</option>
+							<option selected="selected" value="2" data-url="χιλιόμετρα">Χιλιόμετρα (km)</option>
+							<option value="24" data-url="έτη-φωτός">Έτη φωτός (ly)</option>
+							<option value="1" data-url="μέτρα">Μέτρα (m)</option>
+							<option value="10" data-url="μίλια">Μίλια (mi)</option>
+							<option value="20" data-url="μιλς">Μιλς (mil)</option>
+							<option value="5" data-url="χιλιοστόμετρα">Χιλιοστόμετρα (mm)</option>
+							<option value="7" data-url="νανόμετρα">Νανόμετρα (nm)</option>
+							<option value="21" data-url="ναυτικά-μίλια">Ναυτικά μίλια (nmi)</option>
+							<option value="23" data-url="παρσέκ">Παρσέκ (pc)</option>
+							<option value="14" data-url="γυάρδες">Γυάρδες (yd)</option>
+							<option value="6" data-url="μικρόμετρα">Μικρόμετρα (μm)</option>
+							<option value="22" data-url="fathom">Fathom</option>
+							<option value="13" data-url="ράβδοι">Ράβδοι</option>
+							<option value="9" data-url="λεύγες">Λεύγες</option>
+							<option value="11" data-url="furlongs">Furlongs</option>
+					</select>
+	</div>
+	<div class="column-2 tablet-column-2 hidden-mobile center"></div>
+	<div class="column-3 tablet-column-2 mobile-column-2 right">
+		<select tabindex="4" id="unit_to">
+							<option value="8" data-url="άνγκστρομ">Άνγκστρομ (Å)</option>
+							<option value="25" data-url="αστρονομικές-μονάδες">Αστρονομικές μονάδες (au)</option>
+							<option value="4" data-url="εκατοστά">Εκατοστά (cm)</option>
+							<option value="3" data-url="δεκατόμετρα">Δεκατόμετρα (dm)</option>
+							<option value="15" data-url="πόδια">Πόδια (ft)</option>
+							<option value="18" data-url="ίντσες">Ίντσες (in)</option>
+							<option value="2" data-url="χιλιόμετρα">Χιλιόμετρα (km)</option>
+							<option value="24" data-url="έτη-φωτός">Έτη φωτός (ly)</option>
+							<option selected="selected" value="1" data-url="μέτρα">Μέτρα (m)</option>
+							<option value="10" data-url="μίλια">Μίλια (mi)</option>
+							<option value="20" data-url="μιλς">Μιλς (mil)</option>
+							<option value="5" data-url="χιλιοστόμετρα">Χιλιοστόμετρα (mm)</option>
+							<option value="7" data-url="νανόμετρα">Νανόμετρα (nm)</option>
+							<option value="21" data-url="ναυτικά-μίλια">Ναυτικά μίλια (nmi)</option>
+							<option value="23" data-url="παρσέκ">Παρσέκ (pc)</option>
+							<option value="14" data-url="γυάρδες">Γυάρδες (yd)</option>
+							<option value="6" data-url="μικρόμετρα">Μικρόμετρα (μm)</option>
+							<option value="22" data-url="fathom">Fathom</option>
+							<option value="13" data-url="ράβδοι">Ράβδοι</option>
+							<option value="9" data-url="λεύγες">Λεύγες</option>
+							<option value="11" data-url="furlongs">Furlongs</option>
+					</select>
+	</div>
+</div>
+<div class="row result">
+	<p><span class="result-from">&nbsp;</span> <span class="result-unit-from">Χιλιόμετρα</span> <span class="result-equals">=</span> <span class="result-to">&nbsp;</span> <span class="result-unit-to">Μέτρα</span></p>
+</div>
+<script type="text/javascript">
+	var convertData = {
+			fromValue: 0.001000000000000000000000000000,
+			toValue: 1.000000000000000000000000000000,
+			fromUrl: "χιλιόμετρα",
+			toUrl: "μέτρα",
+			langd: "/el",
+			convertSlug: "μετατροπή",
+			toSlug: "σε",
+			folder: "u"
+		}
+		
+	document.addEventListener("DOMContentLoaded", function() {
+		var convert = new Converter(convertData);
+		convert.attach('#converter');
+	});
+</script>
+</form><div id="da-bottom"></div>
+<div class="row">
+<div class="precision">Ακρίβεια: <select id="precision-select">
+	<option data-url="0" value="0">Auto</option>
+	<option data-url="2" value="2">&nbsp;&nbsp;&nbsp;2</option>
+	<option data-url="3" value="3">&nbsp;&nbsp;&nbsp;3</option>
+	<option data-url="4" value="4">&nbsp;&nbsp;&nbsp;4</option>
+	<option data-url="5" value="5">&nbsp;&nbsp;&nbsp;5</option>
+	<option data-url="6" value="6">&nbsp;&nbsp;&nbsp;6</option>
+	<option data-url="7" value="7">&nbsp;&nbsp;&nbsp;7</option>
+	<option data-url="8" value="8">&nbsp;&nbsp;&nbsp;8</option>
+	<option data-url="9" value="9">&nbsp;&nbsp;&nbsp;9</option>
+	<option data-url="10" value="10">&nbsp;&nbsp;10</option>
+	<option data-url="12" value="12">&nbsp;&nbsp;12</option>
+	<option data-url="14" value="14">&nbsp;&nbsp;14</option>
+	<option data-url="16" value="16">&nbsp;&nbsp;16</option>
+	<option data-url="18" value="18">&nbsp;&nbsp;18</option>
+	<option data-url="20" value="20">&nbsp;&nbsp;20</option>
+</select> <small>δεκαδικά ψηφία</small></div></div><div class="row intro"><p class="column-5 tablet-column-4">Μετατρέψετε από Χιλιόμετρα σε Μέτρα. Πληκτρολογήστε το ποσό που θέλετε να μετατρέψετε και πατήστε το κουμπί Μετατροπή.</p>
+<div class="row tabs">
+	<div class="row tab-container">
+		<div class="tab" id="tab-1">
+		</div>
+		<div class="tab active" id="tab-2">
+			<table class="conversion-table">
+												<tr>
+					<td><strong>1</strong> Χιλιόμετρα =&nbsp;<strong>1000</strong>&nbsp;Μέτρα</td>
+					<td><strong>10</strong> Χιλιόμετρα = <strong>10000</strong>&nbsp;Μέτρα</td>
+					<td><strong>2500</strong> Χιλιόμετρα = <strong>2500000</strong>&nbsp;Μέτρα</td>
+				</tr>
+								<tr>
+					<td><strong>2</strong> Χιλιόμετρα =&nbsp;<strong>2000</strong>&nbsp;Μέτρα</td>
+					<td><strong>20</strong> Χιλιόμετρα = <strong>20000</strong>&nbsp;Μέτρα</td>
+					<td><strong>5000</strong> Χιλιόμετρα = <strong>5000000</strong>&nbsp;Μέτρα</td>
+				</tr>
+								<tr>
+					<td><strong>3</strong> Χιλιόμετρα =&nbsp;<strong>3000</strong>&nbsp;Μέτρα</td>
+					<td><strong>30</strong> Χιλιόμετρα = <strong>30000</strong>&nbsp;Μέτρα</td>
+					<td><strong>10000</strong> Χιλιόμετρα = <strong>10000000</strong>&nbsp;Μέτρα</td>
+				</tr>
+								<tr>
+					<td><strong>4</strong> Χιλιόμετρα =&nbsp;<strong>4000</strong>&nbsp;Μέτρα</td>
+					<td><strong>40</strong> Χιλιόμετρα = <strong>40000</strong>&nbsp;Μέτρα</td>
+					<td><strong>25000</strong> Χιλιόμετρα = <strong>25000000</strong>&nbsp;Μέτρα</td>
+				</tr>
+								<tr>
+					<td><strong>5</strong> Χιλιόμετρα =&nbsp;<strong>5000</strong>&nbsp;Μέτρα</td>
+					<td><strong>50</strong> Χιλιόμετρα = <strong>50000</strong>&nbsp;Μέτρα</td>
+					<td><strong>50000</strong> Χιλιόμετρα = <strong>50000000</strong>&nbsp;Μέτρα</td>
+				</tr>
+								<tr>
+					<td><strong>6</strong> Χιλιόμετρα =&nbsp;<strong>6000</strong>&nbsp;Μέτρα</td>
+					<td><strong>100</strong> Χιλιόμετρα = <strong>100000</strong>&nbsp;Μέτρα</td>
+					<td><strong>100000</strong> Χιλιόμετρα = <strong>100000000</strong>&nbsp;Μέτρα</td>
+				</tr>
+								<tr>
+					<td><strong>7</strong> Χιλιόμετρα =&nbsp;<strong>7000</strong>&nbsp;Μέτρα</td>
+					<td><strong>250</strong> Χιλιόμετρα = <strong>250000</strong>&nbsp;Μέτρα</td>
+					<td><strong>250000</strong> Χιλιόμετρα = <strong>250000000</strong>&nbsp;Μέτρα</td>
+				</tr>
+								<tr>
+					<td><strong>8</strong> Χιλιόμετρα =&nbsp;<strong>8000</strong>&nbsp;Μέτρα</td>
+					<td><strong>500</strong> Χιλιόμετρα = <strong>500000</strong>&nbsp;Μέτρα</td>
+					<td><strong>500000</strong> Χιλιόμετρα = <strong>500000000</strong>&nbsp;Μέτρα</td>
+				</tr>
+								<tr>
+					<td><strong>9</strong> Χιλιόμετρα =&nbsp;<strong>9000</strong>&nbsp;Μέτρα</td>
+					<td><strong>1000</strong> Χιλιόμετρα = <strong>1000000</strong>&nbsp;Μέτρα</td>
+					<td><strong>1000000</strong> Χιλιόμετρα = <strong>1000000000</strong>&nbsp;Μέτρα</td>
+				</tr>
+</section>
+</div>
+	</footer>
+	</div>
+		<script type="text/javascript">
+		var perivalon = false;
+		var language = 'el';
+	</script>
+	<script type="text/javascript" src="/app.js?4"></script>
+	<script>
+
+</body>
+</html>
